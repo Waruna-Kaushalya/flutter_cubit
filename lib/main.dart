15f7a5905aeda615cad6dfc0725a25f03f16802b
Waruna-Kaushalya <waruna95610@gmail.com>
2021-10-26 12:16:39 +0530

    provide bloc as globally

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_concepts/logic/cubit/counter_cubit.dart';
 import 'package:flutter_bloc_concepts/presentation/routes/app_routes.dart';
 import 'presentation/screens/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   final AppRoutes _appRoutes = AppRoutes();
+
+  MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: MyHomePage.routeName,
+        onGenerateRoute: _appRoutes.onGenerateRoute,
       ),
-      initialRoute: MyHomePage.routeName,
-      onGenerateRoute: _appRoutes.onGenerateRoute,
     );
-  }
-
-  @override
-  void dispose() {
-    _appRoutes.dispose();
-    super.dispose();
   }
 }
--- lib/presentation/routes/app_routes.dart
+++ lib/presentation/routes/app_routes.dart
@@ -1,47 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bloc_concepts/logic/cubit/counter_cubit.dart';
 import 'package:flutter_bloc_concepts/presentation/screens/home_screen.dart';
 import 'package:flutter_bloc_concepts/presentation/screens/second_screen.dart';
 import 'package:flutter_bloc_concepts/presentation/screens/thrid_screen.dart';
 
 class AppRoutes {
-  final CounterCubit _counterCubit = CounterCubit();
-  final CounterCubit _counterCubitSecond = CounterCubit();
-
   Route? onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case MyHomePage.routeName:
         return MaterialPageRoute(
-          builder: (context) => BlocProvider.value(
-            value: _counterCubit,
-            child: const MyHomePage(),
-          ),
+          builder: (context) => const MyHomePage(),
         );
 
       case SecondPage.routeName:
         return MaterialPageRoute(
-          builder: (context) => BlocProvider.value(
-            value: _counterCubitSecond,
-            child: const SecondPage(),
-          ),
+          builder: (context) => const SecondPage(),
         );
 
       case ThridPage.routeName:
         return MaterialPageRoute(
-          builder: (context) => BlocProvider.value(
-            value: _counterCubit,
-            child: const ThridPage(),
-          ),
+          builder: (context) => const ThridPage(),
         );
 
       default:
         return null;
     }
-  }
-
-  void dispose() {
-    _counterCubit.close();
-    _counterCubitSecond.close();
   }
 }
